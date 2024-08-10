81850b2419df30985c84e14382ef762af0a7698c
Serhii Khobotov <hobbitura@gmail.com>
2024-08-10 16:21:12 +0300

    make build

---
+++ index.e879cdca.js
@@ -0,0 +1,2 @@
+for(var e=document.getElementsByTagName("table")[0].rows,t=0;t<e.length;t++){var l=document.getElementsByTagName("table")[0].rows[t].cells[1].cloneNode(!0);document.getElementsByTagName("table")[0].rows[t].lastElementChild.before(l)}
+//# sourceMappingURL=index.e879cdca.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const trow = document.getElementsByTagName("table")[0].rows;
+for(let i = 0; i < trow.length; i++){
+    const secondColumnCell = document.getElementsByTagName("table")[0].rows[i].cells[1];
+    const copySecondColumnCell = secondColumnCell.cloneNode(true);
+    document.getElementsByTagName("table")[0].rows[i].lastElementChild.before(copySecondColumnCell);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
